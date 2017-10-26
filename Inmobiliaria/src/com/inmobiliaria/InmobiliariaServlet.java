f294a2c6358aad73cf7098a926f42c7f3425cf0a
irazu <irazu@DESKTOP-3ADN22Q.roc.mn.charter.com>
2017-10-25 19:43:04 -0500

    changes to servlet class

--- Inmobiliaria/src/com/inmobiliaria/InmobiliariaServlet.java
+++ Inmobiliaria/src/com/inmobiliaria/InmobiliariaServlet.java
@@ -1,12 +1,12 @@
 package com.inmobiliaria;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+//import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class InmobiliariaServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+public class InmobiliariaServlet /*extends HttpServlet*/ {
+	/*public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
-	}
+	}*/
 }
